955058699b8ccea0732009c9bd707c5e8e9e3e2c
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2016-11-04 12:47:29 +0100

    Cleansed gala

--- src/python/module/__init__.py
+++ src/python/module/__init__.py
@@ -406,52 +406,6 @@ del __extendEdgeContractionGraph
 
 
 
-def __addStaticMethodsToUndirectedGraph():
-
-
-
-
-    G = graph.UndirectedGraph
-    CG = G.EdgeContractionGraph
-
-    def _getGalaContractionOrderSettings(
-        mcMapFactory=CG.MulticutObjective.fusionMoveBasedFactory(),
-        runMcMapEachNthTime=1
-    ):
-        s =  graph.gala.GalaContractionOrderSettingsUndirectedGraph()
-        s.mcMapFactory = mcMapFactory
-        s.runMcMapEachNthTime = int(runMcMapEachNthTime)
-        return s
-
-    G.galaContractionOrderSettings = staticmethod(_getGalaContractionOrderSettings)
-
-
-    def _getGalaSettings(threshold0=0.1, threshold1=0.9, thresholdU=0.1, numberOfEpochs=3, numberOfTrees=100,
-                         contractionOrderSettings = G.galaContractionOrderSettings(),
-                         mapFactory=G.MulticutObjective.fusionMoveBasedFactory(),
-                         perturbAndMapFactory=G.MulticutObjective.fusionMoveBasedFactory()):
-        s =  graph.gala.GalaSettingsUndirectedGraph()
-        s.threshold0 = float(threshold0)
-        s.threshold1 = float(threshold1)
-        s.thresholdU = float(thresholdU)
-        s.numberOfEpochs = int(numberOfEpochs)
-        s.numberOfTrees = int(numberOfTrees)
-        s.contractionOrderSettings = contractionOrderSettings
-        s.mapFactory = mapFactory
-        s.perturbAndMapFactory = perturbAndMapFactory
-        return s
-
-    G.galaSettings = staticmethod(_getGalaSettings)
-
-
-
-    def _getGala(settings = G.galaSettings()):
-        return graph.gala.GalaUndirectedGraph(settings)
-    G.gala = staticmethod(_getGala)
-
-
-
-
 __addStaticMethodsToUndirectedGraph()
 del __addStaticMethodsToUndirectedGraph
 
