dc6f2a972ea17ff73324efd55dda3ed36769078e
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2017-05-31 18:06:13 +0200

    Starting to fix marray

--- include/nifty/marray/marray.hxx
+++ include/nifty/marray/marray.hxx
@@ -10,8 +10,76 @@
 
 
 namespace nifty{
+
+// nifty marray implementation that derives from andres marray
 namespace marray{
-    using namespace andres;
+
+    template<class T, bool isConst, class A> 
+    class View : public andres::View<T, isConst, A> {
+    
+    public:
+    // additional element access
+    #ifdef HAVE_CPP11_STD_ARRAY
+
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 1> & c){ 
+            return this->operator()(c[0]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 2> & c){ 
+            return this->operator()(c[0], c[1]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 3> & c){ 
+            return this->operator()(c[0], c[1], c[2]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 4> & c){ 
+            return this->operator()(c[0], c[1], c[2], c[3]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 5> & c){ 
+            return this->operator()(c[0], c[1], c[2], c[3], c[4]);
+        }
+        template<size_t DIM, class COORD_T>
+        reference operator()(const std::array<COORD_T, DIM> & c){ 
+            return this->operator()(c.begin());
+        }
+
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 1> & c)const{ 
+            return this->operator()(c[0]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 2> & c)const{ 
+            return this->operator()(c[0], c[1]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 3> & c)const{ 
+            return this->operator()(c[0], c[1], c[2]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 4> & c)const{ 
+            return this->operator()(c[0], c[1], c[2], c[3]);
+        }
+        template<class COORD_T>
+        reference operator()(const std::array<COORD_T, 5> & c)const{ 
+            return this->operator()(c[0], c[1], c[2], c[3], c[4]);
+        }
+        template<size_t DIM, class COORD_T>
+        reference operator()(const std::array<COORD_T, DIM> & c)const{ 
+            return this->operator()(c.begin());
+        }
+    
+    #endif
+    };
+
+
+    template<class T, bool isConst, class A>
+    class Marray : public View<T, isConst, A> {
+        
+    };
+
 }
 
 namespace tools{
