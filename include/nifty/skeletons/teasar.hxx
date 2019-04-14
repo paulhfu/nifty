7625aa525bb54fc7502d783e512ae880a89b13c6
Constantin Pape <c.pape@gmx.net>
2019-04-14 14:53:58 +0200

    Start implementation of TEASAR skeletonization WIP

---
+++ include/nifty/skeletons/teasar.hxx
@@ -0,0 +1,15 @@
+#include <algorithm>
+
+namespace nifty {
+namespace skeletons {
+
+    // TODO gather all relevant params and output type(s)
+    template<class SEG>
+    inline void skeletonize(const SEG & seg) {
+
+
+    }
+
+
+}
+}
